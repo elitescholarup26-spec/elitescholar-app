8975e7bd0d8f54b677153c98f8558bda9892b54f
elitescholarup26-spec <elitescholar.up26@gmail.com>
2026-03-26 07:54:54 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ar">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
